8ba0c245ef177439b599681c6d74d0ddc04d2c39
zhuyong <yongzhu2014@outlook.com>
2020-03-28 11:35:19 +0800

    程序计数器

---
+++ src/test/java/com/skylaker/jvm/TestService.java
@@ -0,0 +1,12 @@
+package com.skylaker.jvm;
+
+/**
+ * @author skylaker
+ * @version V1.0 2020/3/28 11:03
+ */
+public class TestService {
+    public static void main(String[] args) {
+        int a = 1 + 3;
+        System.out.println("Yes , JVM !");
+    }
+}
