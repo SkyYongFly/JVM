9059678bde2635922660abdbcd197bb6b9d81082
zhuyong <yongzhu2014@outlook.com>
2020-03-29 00:40:06 +0800

    对象头

---
+++ src/test/java/com/skylaker/jvm/Person.java
@@ -0,0 +1,23 @@
+package com.skylaker.jvm;
+
+/**
+ * @author skylaker
+ * @version V1.0 2020/3/29 0:17
+ */
+public class Person {
+
+    private String name;
+
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/test/java/com/skylaker/jvm/TestService.java
+++ src/test/java/com/skylaker/jvm/TestService.java
@@ -10,6 +10,9 @@ public class TestService {
     private static String name;
     private static Integer age;
 
+    private static Person p = new Person("小丽");
+
+
     public static void main(String[] args) {
         System.out.println("Yes , JVM !");
 
@@ -18,6 +21,25 @@ public class TestService {
         System.out.println(name);
         System.out.println(age);
 
-        TestService service = new com.skylaker.jvm.TestService();
+        TestService service = new TestService();
+        TestService service2 = new TestService();
+        System.out.println(service.p == service2.p);
+    }
+
+    // 同步方法
+    public synchronized void getData(){
+        // TODO
+        System.out.println("操作数据成功！");
+    }
+
+    private static final Object lock = new Object();
+    private static final Object[] lock2 = new Object[1];
+
+    public void getData2(){
+        // 同步代码块
+        synchronized (lock2) {
+            // TODO
+            System.out.println("操作数据成功！");
+        }
     }
 }
