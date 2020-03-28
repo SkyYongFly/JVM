577b7df291a7fe78d33054ca53fa378f27d4da82
zhuyong <yongzhu2014@outlook.com>
2020-03-28 20:49:50 +0800

    对象的创建

--- src/test/java/com/skylaker/jvm/TestService.java
+++ src/test/java/com/skylaker/jvm/TestService.java
@@ -5,9 +5,18 @@ package com.skylaker.jvm;
  * @version V1.0 2020/3/28 11:03
  */
 public class TestService {
+    private static int a;
+    private static double b;
+    private static String name;
+    private static Integer age;
+
     public static void main(String[] args) {
-        int a = 1 + 3;
         System.out.println("Yes , JVM !");
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(age);
 
         TestService service = new com.skylaker.jvm.TestService();
     }
